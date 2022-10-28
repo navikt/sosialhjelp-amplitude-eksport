da3ff1e10642c29f17532f8b0dcdd9fb45c42d33
Ida Onshus <25682301+idaons@users.noreply.github.com>
2022-10-28 12:16:35 +0200

    legger til kommunenr for kontrollgruppe

--- index_kontroll.js
+++ index_kontroll.js
@@ -9,7 +9,7 @@ const userActivityUrl = "https://reops-proxy.intern.nav.no/amplitude/api/2/usera
 
 async function getAllUsers() {
   const csvHeader =
-    "amplitudeId,veileder_lang_tid,sosialtjeneste_flaks,veileder_riktig_mange_kanaler,veileder_antall,veileder_riktig,veileder_service,sosialtjeneste_rettferdig,sprak,veileder_initiativ,veileder_snakker,veileder_kontakt,veileder_respekt,veileder_tillit,mange_kanaler,event_time\n";
+    "amplitudeId,veileder_lang_tid,sosialtjeneste_flaks,veileder_riktig_mange_kanaler,veileder_antall,veileder_riktig,veileder_service,sosialtjeneste_rettferdig,sprak,veileder_initiativ,veileder_snakker,veileder_kontakt,veileder_respekt,veileder_tillit,mange_kanaler,kommunenr, event_time\n";
 
   let csv = csvHeader;
 
@@ -40,6 +40,8 @@ async function getAllUsers() {
             },${event.event_properties.veileder_respekt ?? ""},${
               event.event_properties.veileder_tillit ?? ""
             },${event.event_properties.mange_kanaler ?? ""},${
+                event.event_properties.kommunenavn ?? ""
+            },${
               event.event_time ?? ""
             }\n`;
             csv = csv + csvLine;
